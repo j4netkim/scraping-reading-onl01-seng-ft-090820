30af0544a73b3859e60f93863100d4fb5e1d2f9a
J K <kj688895@gmail.com>
2020-09-28 22:01:07 +0000

    Automatically backed up by Learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -6,4 +6,4 @@ html = open("https://flatironschool.com/")
 Nokogiri::HTML(html)
 doc = Nokogiri::HTML(open("https://flatironschool.com"))
 
-doc.css("headline-26OIBN")
+doc.css(".headline-26OIBN").text

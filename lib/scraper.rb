94713b9e20993903e1027144749bdd217b9f810a
J K <kj688895@gmail.com>
2020-09-28 22:07:21 +0000

    Automatically backed up by Learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -4,6 +4,7 @@ require 'open-uri'
 html = open("https://flatironschool.com/")
 
 Nokogiri::HTML(html)
+doc = Nokogiri::HTML(html)
 doc = Nokogiri::HTML(open("https://flatironschool.com/"))
 
 doc.css(".headline-26OIBN").text
